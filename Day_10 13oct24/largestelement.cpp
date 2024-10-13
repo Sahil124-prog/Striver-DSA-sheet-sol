2adac8d9b3649bdeb59fac5d194a2fad4942ef33
Sahil124-prog <rajputsahil2207@gmail.com>
2024-10-13 19:52:11 +0530

    largest array gfg

---
+++ Day_10 13oct24/largestelement.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        // code here
+        int max=INT_MIN;
+        int n = arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+};
